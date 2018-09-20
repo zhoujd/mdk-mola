e1a360995e05f3e9738320372538f4ef2152ee52
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-20 11:10:12 +0800

    check execute file in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 export DISPLAY=:0.0
 
+if [ ! -e ./zzmediaxapp ]; then
+    echo "no ./zzmediaxapp, please run build script"
+    exit 1
+fi
+
 run_task_1101()
 {
     echo "run task 1101 start ..."
