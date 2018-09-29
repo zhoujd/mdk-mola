a4b5d70c589a760a0d9541fef3365c9dfb5d1e36
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-29 14:37:08 +0800

    more working

--- matrix/matrix_2002.c
+++ matrix/matrix_2002.c
@@ -317,6 +317,11 @@ zzStatus ZZMatrix2002_ProcNextFrame(zzMatrix2002ST  *pSelf)
         pHDRMetaData10->white_point_x = 15635;
         pHDRMetaData10->white_point_y = 16450;
 
+        pHDRMetaData10->max_display_mastering_luminance = 0.0001;
+        pHDRMetaData10->min_display_mastering_luminance = 0.0001;
+        pHDRMetaData10->max_content_light_level         = 0.0001;
+        pHDRMetaData10->max_pic_average_light_level     = 0.0001;
+
         pSelf->pipelineParam.output_hdr_metadata->metadata      = pHDRMetaData10;
         pSelf->pipelineParam.output_hdr_metadata->metadata_size = sizeof(VAHdrMetaDataHDR10);
     }    
