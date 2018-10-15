49cf2c7a6fb248f6ea93229a383323f67b0f75a5
Zachary Zhou <zachary.zhou@hotmail.com>
2018-10-15 17:15:19 +0800

    add more for matrix group

--- zzmediax/framework/matrix_grp.h
+++ zzmediax/framework/matrix_grp.h
@@ -1,3 +1,40 @@
 /* matrix_grp.h
  *
  */
+
+#ifndef INCLUDED_ZZ_MATRIX_GRP_H
+#define INCLUDED_ZZ_MATRIX_GRP_H 1
+
+#include "cmn.h"
+#include "matrix_defs.h"
+#include "va_surf_mng.h"
+
+#define MATRIXGRP_ID    (100)
+
+typedef struct _zzMatrixGRPST zzMatrixGRPST;
+typedef struct _zzMatrixGRPInputParamsST zzMatrixGRPInputParamsST;
+
+struct _zzMatrixGRPST
+{
+    zzMatrixBaseST    base;
+
+
+    zz_list           matrix_list;
+};
+
+
+#define GET_MATRIXGRP(p)  container_of(p, zzMatrixGRPST, base)
+
+zzStatus ZZMatrixGRP_Create(zzMatrixGRPST **ppRet);
+zzStatus ZZMatrixGRP_Release(zzMatrixGRPST *pSelf);
+
+zzStatus ZZMatrixGRP_Init(zzMatrixGRPST *pSelf, zzU16 argc, zz_char **argv,
+                          zzPipeCtrlST  *pPipeCtrl);
+
+zzStatus ZZMatrixGRP_RegistMatrix(zzMatrixGRPST *pSelf, zzMatrixBaseST *pMatrix);
+
+zzStatus ZZMatrixGRP_Help(zzHelpInfoST *pSelf, zzU16 argc, zz_char **argv);
+zzStatus ZZMatrixGRP_HelpRegister();
+
+
+#endif /* INCLUDED_ZZ_MATRIX_GRP_H */
