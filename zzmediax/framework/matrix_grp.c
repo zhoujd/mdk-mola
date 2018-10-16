8f0a9a04bafa0cdf2d892a20f7f8afb6d601b2a0
Zachary Zhou <zachary.zhou@hotmail.com>
2018-10-16 10:39:19 +0800

    add more for matrix group

--- zzmediax/framework/matrix_grp.c
+++ zzmediax/framework/matrix_grp.c
@@ -2,5 +2,151 @@
  *
  */
 
+#include "app_base.h"
 #include "matrix_grp.h"
+#include "task_base.h"
 
+static zzStatus ZZMatrixGRP_Start(zzMatrixBaseST *pSelf);
+static zzStatus ZZMatrixGRP_PartStart(zzMatrixBaseST *pSelf);
+
+static zzStatus ZZMatrixGRP_ParseInputString(zzMatrixGRPST  *pSelf, int nArgNum, char **strInput);
+
+//Matrix Cells
+static zzMatrixCellST matrix_cells[] =
+{
+    {ZZ_EVENT_START,         NULL, ZZMatrixGRP_Start,      NULL},
+    {ZZ_EVENT_PART_START,    NULL, ZZMatrixGRP_PartStart,  NULL},
+};
+
+
+zzStatus ZZMatrixGRP_Help(zzHelpInfoST *pSelf, zzU16 argc, zz_char **argv)
+{
+    zzStatus  sts    = ZZ_ERR_NONE;
+
+    ZZPRINTF("Matrix GRP help\n");
+
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_HelpRegister()
+{
+    zzStatus  sts    = ZZ_ERR_NONE;
+    zzAppST   *pApp  = GetAPP();
+
+    zzHelpInfoST  *pHelpInfo = NULL;
+
+    pHelpInfo = (zzHelpInfoST *)AllocAndZeroMem(sizeof(zzHelpInfoST));
+    if (pHelpInfo == NULL)
+    {
+        ZZPRINTF("==zztest (%s) memory alloc\n", __FUNCTION__);
+        sts = ZZ_ERR_MEMORY_ALLOC;
+        goto END;
+    }
+
+    pHelpInfo->help_id         = MATRIXGRP_ID;
+    pHelpInfo->desc_info       = ZZ_STRING("input one frame by one frame");
+    pHelpInfo->pfnMatrixHelpFn = ZZMatrixGRP_Help;
+
+    sts =  ZZHelp_MatrixHelpAdd(&pApp->help, pHelpInfo);
+    if (sts != ZZ_ERR_NONE)
+    {
+        ZZPRINTF("ZZApp_MatrixHelpAdd  error\n");
+        goto END;
+    }
+
+END:
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_Create(zzMatrixGRPST **ppRet)
+{
+    zzStatus  sts = ZZ_ERR_NONE;
+
+    *ppRet = (zzMatrixGRPST *)AllocAndZeroMem(sizeof(zzMatrixGRPST));
+    if (*ppRet == NULL)
+    {
+        ZZPRINTF("==zztest (%s) memory alloc\n", __FUNCTION__);
+        sts = ZZ_ERR_MEMORY_ALLOC;
+        goto END;
+    }
+
+    (*ppRet)->base.cell_list = matrix_cells;
+    (*ppRet)->base.cell_num  = ARRAY_NUM(matrix_cells);
+    (*ppRet)->base.matrix_id = MATRIXGRP_ID;
+
+
+END:
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_Release(zzMatrixGRPST *pSelf)
+{
+    zzStatus  sts = ZZ_ERR_NONE;
+
+    CHECK_POINTER(pSelf, ZZ_ERR_NULL_PTR);
+
+
+END:
+    return sts;
+}
+
+
+zzStatus ZZMatrixGRP_Init(zzMatrixGRPST *pSelf, zzU16 argc, zz_char **argv,
+                          zzPipeCtrlST  *pPipeCtrl)
+{
+    zzStatus  sts = ZZ_ERR_NONE;
+
+    CHECK_POINTER(pSelf, ZZ_ERR_NULL_PTR);
+
+    sts = ZZMatrixGRP_ParseInputString(pSelf, argc, argv);
+    if (sts != ZZ_ERR_NONE)
+    {
+        ZZPRINTF("ZZMatrixGRP_ParseInputString error\n");
+        goto END;
+    }
+
+
+END:
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_Start(zzMatrixBaseST *pMatrixBase)
+{
+    zzStatus       sts   = ZZ_ERR_NONE;
+    zzMatrixGRPST *pSelf = GET_MATRIXGRP(pMatrixBase);
+
+    ZZDEBUG("Matrix %d Start\n", pSelf->base.matrix_id);
+
+    pMatrixBase->next_event = ZZ_EVENT_PART_END;
+
+END:
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_PartStart(zzMatrixBaseST *pMatrixBase)
+{
+    zzStatus       sts    = ZZ_ERR_NONE;
+    zzMatrixGRPST *pSelf = GET_MATRIXGRP(pMatrixBase);
+
+    ZZDEBUG("Matrix %d PartStart\n", pSelf->base.matrix_id);
+
+    pMatrixBase->next_event = ZZ_EVENT_END;
+
+END:
+    return sts;
+}
+
+zzStatus ZZMatrixGRP_ParseInputString(zzMatrixGRPST  *pSelf, int nArgNum, char **strInput)
+{
+    zzStatus           sts     = ZZ_ERR_NONE;
+    zzU8               i       = 1;
+
+    for (i = 1; i < nArgNum; i++ )
+    {
+        CHECK_POINTER(strInput[i], ZZ_ERR_NULL_PTR);
+        {
+        }
+    }
+
+    return sts;
+}
